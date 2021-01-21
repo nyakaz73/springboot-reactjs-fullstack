25cc23345570958ff09144f6698e964fbbfe6b1d
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-01-21 17:40:28 +0200

    Updated main Apps.js

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../../../frontend/src/components/layout/Header';
 import RegisterUser from '../../../frontend/src/components/register-user/RegisterUser';
+import Users from '../../../frontend/src/components/users/Users';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        axios.get('/user/all')
+            .then(response => setUsers(response.data))
+    }
+
+    const removeUser = (id) => {
+
+        axios.delete(`/user/${id}`)
+            .then(
+                () => {
+                    setUsers(
+                        users.filter(
+                            (user) => user.id !== id
+                        )
+                    )
+                }
+            );
+    }
+
+    const addUser = (newUser) => {
+        axios.post('/user/save', newUser)
+            .then(
+                (response) => {
+                    setUsers([...users, response.data])
+                }
+            );
+    }
+
+
     return (
         <div>
             <Header />
-            <RegisterUser />
+            <RegisterUser addUser={addUser} />
+            <Users users={users} removeUser={removeUser} />
         </div>
     )
 }
