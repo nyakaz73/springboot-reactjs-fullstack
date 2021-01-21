acf92e9202b1e6ca8663e0318b21cc424ee2810d
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-01-21 17:39:11 +0200

    Created the users component

---
+++ frontend/src/components/users/Users.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import UserInfo from '../user-info/UserInfo';
+import PropTypes from "prop-types";
+const Users = ({ users, removeUser }) => {
+
+    return (
+        <div style={container}>
+            {
+                users.map(user => (
+                    <UserInfo user={user} key={user.id} removeUser={removeUser} />
+                ))
+            }
+        </div>
+    )
+}
+
+
+const container = {
+    border: '2px solid #544073',
+    margin: '50px 50px 80px 50px'
+}
+
+Users.propTypes = {
+    users: PropTypes.array,
+    removeUser: PropTypes.func.isRequired
+}
+export default Users;
