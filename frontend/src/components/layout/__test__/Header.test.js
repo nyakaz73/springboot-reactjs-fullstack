168151d8574d9eb5d72a4649964ec987d1302eb7
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-02-01 00:52:59 +0200

    Updated Header test with snapshots and props tests

--- frontend/src/components/layout/__test__/Header.test.js
+++ frontend/src/components/layout/__test__/Header.test.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom"
 import { cleanup, act } from "@testing-library/react";
 import Header from "../Header";
 
+//for Snapshot testing
+import renderer from 'react-test-renderer';
+
 //unmount and clean DOM after every test
 //afterEach(cleanup);
 let container;
@@ -24,4 +27,24 @@ it("renders Header without crashing", () => {
         ReactDOM.render(<Header />, container)
     })
     expect(container.textContent).toBe("User Registration")
+})
+
+it("renders Header with a prop", () => {
+    //Paint the DOM with component
+    const title = "My Header";
+    act(() => {
+        ReactDOM.render(<Header title={title} />, container)
+    })
+    expect(container.textContent).toBe("My Header")
+})
+
+it("matches snapshot", () => {
+    const tree = renderer.create(<Header />).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+it("matches snapshot with a prop", () => {
+    const title = "My title"
+    const tree = renderer.create(<Header title={title} />).toJSON();
+    expect(tree).toMatchSnapshot();
 })
