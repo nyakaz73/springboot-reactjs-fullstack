25c93ddc6924d3bd5dfee8d7d4ee8a32bc1bf444
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-02-01 23:17:28 +0200

    Handlemyfirst Input Test

--- frontend/src/components/register-user/__test__/RegisterUser.test.js
+++ frontend/src/components/register-user/__test__/RegisterUser.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { act } from "@testing-library/react";
+import { act, fireEvent, render } from "@testing-library/react";
 import RegisterUser from "../RegisterUser";
 import renderer from "react-test-renderer"
 
@@ -20,4 +20,17 @@ it("renders RegisterUser without crashing", () => {
     act(() => {
         ReactDOM.render(<RegisterUser />, container)
     })
+
 })
+
+const setup = () => {
+
+}
+//To test the state of the form we use Enzyme simulate or React Testing Lib simulate events
+it("update on onChange state on input form", () => {
+    const utils = render(<RegisterUser />);
+    const input = utils.getByPlaceholderText('Name');
+    fireEvent.change(input, { target: { value: 'John' } })
+    expect(input.value).toBe("John")
+})
+
