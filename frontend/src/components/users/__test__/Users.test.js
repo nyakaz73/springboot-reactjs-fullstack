f69e0101e5db4730d180599e23c09a05974893f8
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2021-02-02 18:07:26 +0200

    Updated Users test

--- frontend/src/components/users/__test__/Users.test.js
+++ frontend/src/components/users/__test__/Users.test.js
@@ -19,12 +19,46 @@ afterEach(() => {
 
 it("Renders <Users/> without crashing", () => {
     const removeUser = jest.fn()
-    const users = []
-    render(<Users users={users} removeUser={removeUser} />)
+    const users = [
+        {
+            id: "1",
+            name: "John",
+            surname: "Marcus",
+            username: "johnny",
+            email: "john@gmail.com",
+            password: "some-strong-password",
+        },
+        {
+            id: "2",
+            name: "Peter",
+            surname: "Rubel",
+            username: "peter",
+            email: "peter@gmail.com",
+            password: "some-strong-password2",
+        },
+        {
+            id: "3",
+            name: "Daniel",
+            surname: "James",
+            username: "daniel",
+            email: "daniel@gmail.com",
+            password: "some-strong-password3",
+        },
+    ];
+    act(() => {
+        ReactDOM.render(<Users users={users} removeUser={removeUser} />, container);
+    });
+    expect(container.textContent).toContain(users[0].email);
+    expect(container.textContent).toContain(users[0].name);
+    expect(container.textContent).toContain(users[0].surname);
+    expect(container.textContent).toContain(users[1].email);
+    expect(container.textContent).toContain(users[2].email);
 })
 
 it("matches snapshot", () => {
-    const tree = renderer.create(<Users />).toJSON();
+    const removeUser = jest.fn()
+    const users = []
+    const tree = renderer.create(<Users users={users} removeUser={removeUser} />).toJSON();
     expect(tree).toMatchSnapshot();
 
 })
